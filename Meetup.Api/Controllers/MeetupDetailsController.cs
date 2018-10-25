d4fe0d145ae8d5340d3035f21d3c3eebe2cb6f02
Vinod Gupta <vkg.codefactory@gmail.com>
2018-10-25 23:42:22 +0800

    code organization

--- Meetup.Api/Controllers/MeetupDetailsController.cs
+++ Meetup.Api/Controllers/MeetupDetailsController.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+
+using Meetup.Entities.Models;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Meetup.Entities.Models;
 
 [assembly: ApiConventionType(typeof(DefaultApiConventions))]
 
--- Meetup.Api/Startup.cs
+++ Meetup.Api/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Meetup.Entities.Models;
+﻿using Meetup.Entities.Models;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace Meetup.Api
